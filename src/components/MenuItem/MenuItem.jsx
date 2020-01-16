7e4e3e9934020c9de626f19ef72c74e59f982173
Muhammed Gaber <mohammedgaber273@gmail.com>
2020-01-16 05:35:15 +0200

    added some styles to the MenuItem for hovering and background

--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -1,21 +1,17 @@
 import React from "react";
 import "./MenuItem.scss";
-import { CardImgOverlay } from "reactstrap";
 
 const MenuItem = ({ title, imageUrl, size }) => {
   return (
-    <div
-      // for dynamic styling we can use the style property
-      style={{
-        backgroundImage: `url(${imageUrl})`
-      }}
-      // if there is a size property then it will get added to the className
-      // otherwise nothing is going to change, then in the styles sheet
-      // we can add some styles to this class if it exists
-      className={`${size} menu-item`}
-    >
+    <div className={`${size} menu-item`}>
+      <div
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+        className="background-image"
+      ></div>
       <div className="content">
-        = <h2 className="title"> {title} </h2>
+        <h2 className="title"> {title.toUpperCase()} </h2>
         <span className="subtitle">shop now</span>
       </div>
     </div>
