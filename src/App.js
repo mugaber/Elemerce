5ffe502d4cd18e14d99244416485edc008d9a2f7
Muhammed Gaber <mohammedgaber273@gmail.com>
2020-01-17 04:54:02 +0200

    created createUserProfileDocument auth

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import SignInSignUp from "./pages/SignInSignUp/SignInSignUp";
 import HomePage from "./pages/HomePage/HomePage";
 import { Switch, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
-import { auth } from "./firebase/firebase.utils";
 import React, { Component } from "react";
 import ShopPage from "./pages/shop/shop";
 
@@ -18,9 +18,26 @@ class App extends Component {
   unSubscribeFromAuth = null;
 
   componentDidMount() {
-    auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    // when there is a change in the auth from firebase (signin with google)
+    // call creerateuserprofile and update the state of the current user
+    this.unSubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      // when logging in will return userAuth object
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        // take a snapshot from the userRef object that have the info
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        });
+        // when logging out the auth.onChange will return null
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
     });
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -23,6 +23,39 @@ provider.setCustomParameters({ prompt: "select_account" });
 
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
-// activate the google auth in firebase 
+// if the user logged in create a profile and save it in the firestore
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  // if the user is logging out
+  if (!userAuth) return;
+
+  // to qurey from the firestore (database) from the collection users
+  // return an object respresenting the user even if does not exists
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  // document Snapshot that represents the data, ex. exists
+  const snapShot = await userRef.get();
+
+  // if there is the first time
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      // query to firebase to set this user in database with this info
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log("Error creating user", error.message);
+    }
+  }
+
+  return userRef;
+};
+
+// activate the google auth in firebase
 
 export default firebase;
